2d918124f3298b560602354dc9ad01b69fdca181
Sandaru Fernando <sandarustar7@gmail.com>
2020-02-02 00:35:28 -0600

    Added Pet

--- src/HelloWorld/HelloWorld.java
+++ src/HelloWorld/HelloWorld.java
@@ -1,9 +1,5 @@
 package HelloWorld;
-
-import java.nio.file.Paths;
-
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-    }
-}
+    }}
